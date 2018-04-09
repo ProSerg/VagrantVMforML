2bc2389628fc9c1a8764bdc90482a6819cc5af2a
Markin Sergey <smarkin@vrconcept.net>
2018-04-09 11:20:34 +0300

    init projects

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# VagrantVMforML
+The guest machine entered an invalid state while waiting for it
+to boot. Valid states are 'starting, running'. The machine is in the
+'unknown' state. Please verify everything is configured
+properly and try again.
+
+If the provider you're using has a GUI that comes with it,
+it is often helpful to open that and watch the machine, since the
+GUI often has more helpful error messages than Vagrant can retrieve.
+For example, if you're using VirtualBox, run `vagrant up` while the
+VirtualBox GUI is open.
+
+The primary issue for this error is that the provider you're using
+is not properly configured. This is very rarely a Vagrant issue.
+
+
+
+# todo
+
+1. requrement.txt
+2. swap
---
+++ scripts/setup.sh
@@ -0,0 +1,102 @@
+# Variables
+NotebooksDir="/notebooks/"
+# size of swapfile in megabytes
+swapsize=8000
+
+echo ">>> Provisioning virtual machine..."
+apt-get update
+
+# Git
+# does the git file already exist?
+command -v git 
+if [ $? -ne 0 ]; then
+    echo ">>> Installing Git"
+    apt-get install -y git 
+fi
+
+# does the Docker file already exist?
+command -v docker 
+if [ $? -ne 0 ]; then
+    # Docker
+    echo ">>> Installing Docker" 
+    apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    apt-key fingerprint 0EBFCD88
+    add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+    apt-get update
+    apt-get install -y docker-ce
+    apt-cache madison docker-ce
+fi
+
+# does the python3.6 file already exist?
+command -v python3.6
+if [ $? -ne 0 ]; then
+    # Python 3
+    echo ">>> Installing Python 3.6"
+    add-apt-repository ppa:deadsnakes/ppa
+    apt-get update
+    apt-get install -y python3.6
+    curl https://bootstrap.pypa.io/get-pip.py | python3.6
+    apt-get install python3.6-venv
+    pip3.6 install -U pip 
+
+    update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1
+    update-alternatives --install /usr/bin/python python /usr/bin/python3.6 10
+fi 
+
+echo ">>> Installing ML libraries"
+
+# does the jupyter file already exist?
+command -v jupyter 
+if [ $? -ne 0 ]; then
+    echo ">>> Installing Jupyter"
+    python -m pip install jupyter
+    jupyter notebook --generate-config -y
+
+    cp -r /data/jupyter.service /etc/systemd/system
+
+    mkdir -p /home/vagrant/.jupyter/
+    cp -r /data/jupyter_notebook_config.py /home/vagrant/.jupyter/
+fi
+echo ">>> Demonization Jupyter"
+
+systemctl enable jupyter.service
+systemctl daemon-reload
+systemctl restart jupyter.service
+systemctl status jupyter
+jupyter notebook list
+
+echo "Jupyter == http://localhost:8888/?token=sha1:cf2a799f39bc:aad334f602bf3425668db12f7170e8048eac99d6"
+# ifconfig -a | grep 'inet ' | grep -v '0.1'  | awk '{print $2}' | cut -f1 -d'/'
+
+echo ">>> Finished setuping."
+
+
+# does the swap file already exist?
+grep -q "swapfile" /etc/fstab
+
+# if not then create it
+if [ $? -ne 0 ]; then
+  echo '>>> swapfile not found. Adding swapfile.'
+  fallocate -l ${swapsize}M /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+  echo 'swapfile found. No changes made.'
+fi
+
+# output results to terminal
+df -h
+cat /proc/swaps
+cat /proc/meminfo | grep Swap
